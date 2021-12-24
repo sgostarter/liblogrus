2bcf1672622c085d995daa825fe399e965dd62e7
patdz <patdz@ymipro.com>
2021-12-24 11:27:34 +0800

    init

--- liblogrus_test.go
+++ liblogrus_test.go
@@ -3,14 +3,16 @@ package liblogrus
 import (
 	"testing"
 
+	"github.com/sgostarter/i/l"
 	"github.com/sgostarter/i/logger"
 )
 
 func Test1(t *testing.T) {
 	rl := NewLogrus()
 	rl.SetLevel(logger.LevelInfo)
-	log := logger.NewWrapper(NewLogrus())
 
-	log.WithFields(logger.FieldString("s1k", "s1v")).Info("ddd")
-	log.WithFields(logger.FieldString("s1k", "s1v"), logger.FieldAny("int", 9887)).Info("ddd")
+	log := l.NewWrapper(NewLogrus())
+
+	log.WithFields(l.StringField("s1k", "s1v")).Info("ddd")
+	log.WithFields(l.StringField("s1k", "s1v"), l.IntField("int", 9887)).Info("ddd")
 }
